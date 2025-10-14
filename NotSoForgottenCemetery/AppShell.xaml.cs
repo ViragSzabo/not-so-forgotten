f6dc343d6c9ef58d35550a15ecfd48ae2a368cef
ViragSzabo <virag.szabo@student.nhlstenden.com>
2025-10-14 14:23:44 +0200

    🕯️ Initial MAUI project setup 🕯️

---
+++ NotSoForgottenCemetery/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotSoForgottenCemetery
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NotSoForgottenCemetery/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NotSoForgottenCemetery
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
